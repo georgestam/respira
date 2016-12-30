fdf04fc89cb120d24d1977d2550c90a3df01de96
Jordi Fernandez <jordi@ywarp.com>
2016-12-30 17:20:26 +0000

    project template complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,14 @@
 //= require owl-carousel/owl.carousel.js
 //= require jquery.browser.js
 //= require SmoothScroll.js
+//= require vide/jquery.vide.js
+//= require morphext/morphext.min.js
+//= require jquery.parallax-1.1.3.js
+//= require isotope/isotope.pkgd.min.js
+//= require rs-plugin/js/jquery.themepunch.revolution.min.js
+//= require rs-plugin/js/jquery.themepunch.tools.min.js
+//= require modernizr.js
+
 
 //= require js/template.js
 //= require js/custom.js
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :index ]
 
   def home
+  end
+
+  def index
   end
 end

5768eb6e46a95637b1bb3b34792bf4f5cc496fc9
Jordi Fernandez <jordi@ywarp.com>
2016-12-30 15:33:43 +0000

    add stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,18 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+//= require modernizr.js
+//= require magnific-popup/jquery.magnific-popup.min.js
+//= require waypoints/jquery.waypoints.min.js
+//= require jquery.countTo.js
+//= require jquery.parallax-1.1.3.js
+//= require jquery.validate.js
+//= require owl-carousel/owl.carousel.js
+//= require jquery.browser.js
+//= require SmoothScroll.js
+
+//= require js/template.js
+//= require js/custom.js
+

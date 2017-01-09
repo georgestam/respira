caab2921e6ae77c2dd067b086f8eaa327ad3387f
Jordi Fernandez <jordi@ywarp.com>
2017-01-09 20:45:43 +0000

    main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,31 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :index ]
 
   def index
+    x = [0,2,4,6]
+
+
+    @result = pair_elements(x)
   end
+
+end
+
+
+def pair_elements(array)
+
+    new_array = []
+    i = 0
+    array.each do |int|
+        x = 1
+        size_array = array.size - 1
+        size_array.times do
+          new_array << [int, array[i+x] ]
+
+          x += 1
+        end
+
+        i += 1
+    end
+
+    new_array
 
 end

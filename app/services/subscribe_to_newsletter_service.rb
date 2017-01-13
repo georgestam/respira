59de44362a11820ded5ef3434c3400531ba17f64
Jordi Fernandez <jordi@ywarp.com>
2017-01-13 22:29:10 +0000

    minor changes

--- app/services/subscribe_to_newsletter_service.rb
+++ app/services/subscribe_to_newsletter_service.rb
@@ -10,10 +10,10 @@ class SubscribeToNewsletterService
       body: {
         email_address: @user.email,
         status: "subscribed",
-        # merge_fields: {
-        #   FNAME: @user.first_name,
-        #   LNAME: @user.last_name
-        # }
+        merge_fields: {
+          FNAME: @user.name
+          # LNAME: @user.last_name
+        }
       }
     )
   end

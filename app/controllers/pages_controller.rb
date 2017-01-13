0c9d1ed354f6a83c0d6a2288edbe171e9446f2c8
Jordi Fernandez <jordi@ywarp.com>
2017-01-13 12:16:43 +0000

    add devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :index ]
+  skip_before_action :authenticate_user!, only: [ ]
 
   def index
     x = [0,2,4,6]

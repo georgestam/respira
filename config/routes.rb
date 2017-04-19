c87c286689f52613355dd355b100fe7d2de82736
Jordi Fernandez <jordi@ywarp.com>
2017-04-19 21:13:27 +0200

    adjust home page to show 3 languages and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  root to: 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  scope '(:locale)', locale: /en|ea|es/ do
+  
+    root to: 'pages#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :pages, only:[:index, :create, :destroy]
+    resources :pages, only:[:index, :create, :destroy]
+  
+  end
 
 
 end

d5fcd85a485e98d663543b5a09ce2c5e06c99347
vemv <vemv@users.noreply.github.com>
2018-04-28 23:39:31 +0200

    Minor cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, email_format: { message: "doesn't look like an email address" }, presence: true
   validates :name, presence: true
